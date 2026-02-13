52294903946f3eb05da30467dd9530190a7f89c0
Jordan Robin <jordanrobin100@gmail.com>
2026-02-13 12:04:38 +0100

    feat(core): initialisation du repo

---
+++ src/main/java/com/jordanrobin/financial_erp/FinancialErpApplication.java
@@ -0,0 +1,13 @@
+package com.jordanrobin.financial_erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jordanrobin/financial_erp/FinancialErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordanrobin.financial_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
