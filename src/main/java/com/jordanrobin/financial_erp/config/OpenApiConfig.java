1af38c3f1406c2c0c3ef51a5cb2f4f35c0d4830d
Jordan Robin <jordanrobin100@gmail.com>
2026-03-10 09:56:56 +0100

    feat(core): ajout de l'autorisation token JWT dans Swagger

--- src/main/java/com/jordanrobin/financial_erp/config/OpenApiConfig.java
+++ src/main/java/com/jordanrobin/financial_erp/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.jordanrobin.financial_erp.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,15 +15,25 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("Financial ERP API")
-                        .version("1.0.0")
-                        .description("API REST pour l'ERP financier SaaS pour PME")
-                        .contact(new Contact()
-                                .name("Jordan Robin")
-                                .email("contact@example.com"))
-                        .license(new License()
-                                .name("Proprietary")));
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+            .components(new Components()
+                .addSecuritySchemes(securitySchemeName,
+                    new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
+            .info(new Info()
+                .title("Financial ERP API")
+                .version("1.0.0")
+                .description("API REST pour l'ERP financier SaaS pour PME")
+                .contact(new Contact()
+                    .name("Jordan Robin")
+                    .email("contact@example.com"))
+                .license(new License()
+                    .name("Proprietary")));
     }
 }
